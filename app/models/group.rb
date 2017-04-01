2f1479fb98ba4619bab2239cea17436ca846a9ad
jkxruby <jkxruby@gmail.com>
2017-04-01 22:36:48 +0800

    add post and hook group/user relation

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,7 @@
 class Group < ApplicationRecord
   belongs_to :user
+
+  has_many :posts
+
   validates :title, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
   has_many :groups
+  
+  has_many :posts
 end
