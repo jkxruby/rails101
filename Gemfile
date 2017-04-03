1fb8e87770204401cebfcce6368c342a3eb4409e
jkxruby <jkxruby@gmail.com>
2017-04-03 11:30:44 +0800

    implement posts pagination with will_paginate

--- Gemfile
+++ Gemfile
@@ -39,6 +39,7 @@ gem 'bootstrap-sass'
 gem 'simple_form'
 
 gem 'devise'
+gem 'will_paginate'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -8,7 +8,7 @@ before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
 
      def show
    @group = Group.find(params[:id])
-   @posts = @group.posts.recent
+   @posts = @group.posts.recent.paginate(:page => params[:page], :per_page => 5)
  end
 
   def edit
