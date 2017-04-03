ccbe5be0091d9d08309b5d218e4422c9b008622f
jkxruby <jkxruby@gmail.com>
2017-04-03 14:21:11 +0800

    join & quit group action

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,6 +1,6 @@
 class GroupsController < ApplicationController
 
-before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
+before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destory, :join , :quit]
 before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
   def index
       @groups = Group.all
@@ -47,6 +47,32 @@ before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
     @group.destroy
     redirect_to groups_path, alert: "Group deleted"
   end
+
+  def join
+     @group = Group.find(params[:id])
+
+      if !current_user.is_member_of?(@group)
+        current_user.join!(@group)
+        flash[:notice] = "加入本讨论版成功！"
+      else
+        flash[:warning] = "你已经是本讨论版成员了！"
+      end
+
+      redirect_to group_path(@group)
+    end
+
+    def quit
+      @group = Group.find(params[:id])
+
+      if current_user.is_member_of?(@group)
+        current_user.quit!(@group)
+        flash[:alert] = "已退出本讨论版！"
+      else
+        flash[:warning] = "你不是本讨论版成员，怎么退出 XD"
+      end
+
+      redirect_to group_path(@group)
+    end
 
   private
   def find_group_and_check_permission
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :groups do
+
+    member do
+          post :join
+          post :quit
+        end
+
     resources :posts
   end
   root 'groups#index'
